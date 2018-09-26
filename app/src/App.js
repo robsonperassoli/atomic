f9f0e19aedc73484bb3fee2d23828def4c459a8d
Robson Gian Perassoli <robsonperassoli@gmail.com>
2018-09-26 10:58:22 -0300

    remove default css

--- app/src/App.js
+++ app/src/App.js
@@ -1,23 +1,32 @@
-import React, { Component } from 'react';
-import { Switch, Route } from 'react-router'
-import Header from './Header';
+import React, { Component } from 'react'
+import { Switch, Route, Redirect, withRouter } from 'react-router'
 import LoginPage from './pages/LoginPage'
 import HomePage from './pages/HomePage'
-import './App.css';
+import './App.css'
 
 class App extends Component {
   render() {
+    const { location } =  this.props
+    const isLoginPage = location.pathname === '/login'
+    const loggedIn = false;
     return (
       <div className='atomic'>
-        <Header />
+        
+        {(!isLoginPage && !loggedIn) && (
+          <Redirect to='/login' />
+        )}
+
+        {(isLoginPage && loggedIn) && (
+          <Redirect to='/' />
+        )}
 
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route exact path='/login' component={LoginPage} />
         </Switch>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default withRouter(App)
