6cffeec9fdafc00b1a9b788467b7483c33be8fcb
Robson Gian Perassoli <robsonperassoli@gmail.com>
2018-12-27 02:30:54 -0200

    make app layout a hoc

--- app/src/pages/HomePage.js
+++ app/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import { Segment, Menu } from 'semantic-ui-react'
 import styled from 'styled-components'
 import gql from 'graphql-tag'
 import { Query } from 'react-apollo'
-import { AppLayout } from '../layouts'
+import withAppLayout  from '../components/hocs/withAppLayout'
 
 const GET_TASKS = gql`
   query GetTasks($projectId: ID!) {
@@ -22,48 +22,34 @@ const GET_TASKS = gql`
   }
 `
 
-const SELECTED_PROJECT = gql`
-  {
-    selectedProjectId @client
-  }
-`
-
 const Container = styled.div`
   margin-top: 20px;
 `
 
 const HomePage = ({ selectedProjectId }) => (
-    <AppLayout>
-      <Query query={GET_TASKS} variables={{ projectId: selectedProjectId }}>
-        {({ loading, data: { project }}) => loading ? null : (
-          <Container>
-            <Menu attached='top' widths={7} size='huge'>
-              <Menu.Item name='sunday' active={false} onClick={() => null}>Sun</Menu.Item>
-              <Menu.Item name='monday' active={false} onClick={() => null}>Mon</Menu.Item>
-              <Menu.Item name='tuesday' active={false} onClick={() => null}>Tue</Menu.Item>
-              <Menu.Item name='wednesday' active onClick={() => null}>Wed</Menu.Item>
-              <Menu.Item name='thursday' active={false} onClick={() => null}>Thu</Menu.Item>
-              <Menu.Item name='friday' active={false} onClick={() => null}>Fri</Menu.Item>
-              <Menu.Item name='saturday' active={false} onClick={() => null}>Sat</Menu.Item>
-            </Menu>
+  <Query query={GET_TASKS} variables={{ projectId: selectedProjectId }}>
+    {({ loading, data: { project }}) => loading ? null : (
+      <Container>
+        <Menu attached='top' widths={7} size='huge'>
+          <Menu.Item name='sunday' active={false} onClick={() => null}>Sun</Menu.Item>
+          <Menu.Item name='monday' active={false} onClick={() => null}>Mon</Menu.Item>
+          <Menu.Item name='tuesday' active={false} onClick={() => null}>Tue</Menu.Item>
+          <Menu.Item name='wednesday' active onClick={() => null}>Wed</Menu.Item>
+          <Menu.Item name='thursday' active={false} onClick={() => null}>Thu</Menu.Item>
+          <Menu.Item name='friday' active={false} onClick={() => null}>Fri</Menu.Item>
+          <Menu.Item name='saturday' active={false} onClick={() => null}>Sat</Menu.Item>
+        </Menu>
 
-            {project.tasks.map(task => (
-              <Segment key={task.id} attached>
-                {task.description}
-              </Segment>
-            ))}
+        {project.tasks.map(task => (
+          <Segment key={task.id} attached>
+            {task.description}
+          </Segment>
+        ))}
 
-            <Segment attached='bottom'>3:20</Segment>
-          </Container>
-        )}
-      </Query>
-    </AppLayout>
-)
-
-export default (props) => (
-  <Query query={SELECTED_PROJECT}>
-    {({ data: { selectedProjectId }}) => (
-      <HomePage selectedProjectId={selectedProjectId} {...props} />
+        <Segment attached='bottom'>3:20</Segment>
+      </Container>
     )}
   </Query>
 )
+
+export default withAppLayout(HomePage)
