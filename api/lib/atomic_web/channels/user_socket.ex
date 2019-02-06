86a2c3b5973d4f33288e5db1a0607f5e7e10cb0d
Robson Gian Perassoli <robsonperassoli@gmail.com>
2019-02-06 18:37:58 -0200

    allow socket to accept connections from netlify

--- api/lib/atomic_web/channels/user_socket.ex
+++ api/lib/atomic_web/channels/user_socket.ex
@@ -9,7 +9,8 @@ defmodule AtomicWeb.UserSocket do
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket,
-    timeout: 45_000
+    timeout: 45_000,
+    check_origin: ["https://atomic-time.netlify.com"]
   # transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
