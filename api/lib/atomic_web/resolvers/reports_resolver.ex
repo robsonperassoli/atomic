f0faf6b793e7adb3defd60a7ca3eef1747f4e5cc
Robson Gian Perassoli <robsonperassoli@gmail.com>
2020-01-21 19:14:32 -0300

    fix report url

--- api/lib/atomic_web/resolvers/reports_resolver.ex
+++ api/lib/atomic_web/resolvers/reports_resolver.ex
@@ -1,7 +1,7 @@
 defmodule AtomicWeb.ReportsResolver do
   alias Atomic.Reports
 
-  @base_url "http://localhost:4000/api/reports"
+  def base_url, do: "#{AtomicWeb.Endpoint.url()}/api/reports"
 
   def random_string(length) do
     :crypto.strong_rand_bytes(length) |> Base.url_encode64 |> binary_part(0, length)
@@ -10,9 +10,10 @@ defmodule AtomicWeb.ReportsResolver do
   def request_tasks_report(_root, args, %{context: %{current_user: user}}) do
     %{ start_date: start_date, end_date: end_date, project_id: project_id } = args
 
+
     id = random_string(32)
     title = "Task report #{start_date} to #{end_date}"
-    report = %{id: id, title: title, url: "#{@base_url}/tasks?project_id=#{project_id}&start_date=#{start_date}&end_date=#{end_date}"}
+    report = %{id: id, title: title, url: "#{base_url()}/tasks?project_id=#{project_id}&start_date=#{start_date}&end_date=#{end_date}"}
 
     Absinthe.Subscription.publish(AtomicWeb.Endpoint, report, report_finished: "user:#{user.id}")
     {:ok, true}
